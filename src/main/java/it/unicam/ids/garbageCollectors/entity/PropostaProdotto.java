da0c61cb5499434994931d2693964d3e7f7528ec
Davide Belfiori <davidebelfiori@gmail.com>
2019-02-12 11:02:22 +0100

    Aggiunta data creazione

--- src/main/java/it/unicam/ids/garbageCollectors/entity/PropostaProdotto.java
+++ src/main/java/it/unicam/ids/garbageCollectors/entity/PropostaProdotto.java
@@ -1,15 +1,14 @@
 package it.unicam.ids.garbageCollectors.entity;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import it.unicam.ids.garbageCollectors.entity.id.PropostaProdottoId;
 import lombok.Data;
@@ -31,4 +30,9 @@ public class PropostaProdotto {
 	@Column(name = "nome_prodotto")
 	@NotNull
 	private String nomeProdotto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created")
+	@NotNull
+	private Date created = new Date();
 }
